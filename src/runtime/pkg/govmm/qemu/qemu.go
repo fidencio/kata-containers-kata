ab326b29311f74f899101636defc51647c589927
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2023-02-23 14:33:47 +0100

    runtime/qemu: Set the TDVF as a BIOS
    This is needed since 22ww49.
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- src/runtime/pkg/govmm/qemu/qemu.go
+++ src/runtime/pkg/govmm/qemu/qemu.go
@@ -368,12 +368,7 @@ func (object Object) QemuParams(config *Config) []string {
 		if object.Debug {
 			objectParams = append(objectParams, "debug=on")
 		}
-		deviceParams = append(deviceParams, string(object.Driver))
-		deviceParams = append(deviceParams, fmt.Sprintf("id=%s", object.DeviceID))
-		deviceParams = append(deviceParams, fmt.Sprintf("file=%s", object.File))
-		if object.FirmwareVolume != "" {
-			deviceParams = append(deviceParams, fmt.Sprintf("config-firmware-volume=%s", object.FirmwareVolume))
-		}
+		config.Bios = object.File
 	case SEVGuest:
 		objectParams = append(objectParams, string(object.Type))
 		objectParams = append(objectParams, fmt.Sprintf("id=%s", object.ID))
