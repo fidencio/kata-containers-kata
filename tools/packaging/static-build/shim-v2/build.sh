315f4ef63758729fc40185ab73eaca01021f51b3
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2023-03-23 18:47:52 +0100

    HACK: do not build the runtime-rs
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- tools/packaging/static-build/shim-v2/build.sh
+++ tools/packaging/static-build/shim-v2/build.sh
@@ -24,7 +24,7 @@ container_image="${SHIM_V2_CONTAINER_BUILDER:-$(get_shim_v2_image_name)}"
 sudo docker pull ${container_image} || \
        	(sudo docker build  \
 		--build-arg GO_VERSION="${GO_VERSION}" \
-		--build-arg RUST_VERSION="${RUST_VERSION}" \
+		--build-arg RUST_VERSION="" \
 		-t "${container_image}" \
 		"${script_dir}" && \
 	 push_to_registry "${container_image}")
@@ -34,16 +34,6 @@ if [ ${arch} = "ppc64le" ]; then
 	arch="ppc64"
 fi
 
-sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
-	-w "${repo_root_dir}/src/runtime-rs" \
-	"${container_image}" \
-	bash -c "git config --global --add safe.directory ${repo_root_dir} && make PREFIX=${PREFIX} QEMUCMD=qemu-system-${arch}"
-
-sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
-	-w "${repo_root_dir}/src/runtime-rs" \
-	"${container_image}" \
-	bash -c "git config --global --add safe.directory ${repo_root_dir} && make PREFIX="${PREFIX}" DESTDIR="${DESTDIR}" install"
-	
 sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
 	-w "${repo_root_dir}/src/runtime" \
 	"${container_image}" \
