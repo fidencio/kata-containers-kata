a20892859739943324f08227d61f87ac8f350ebf
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2023-03-23 23:28:59 +0100

    payload: Make it Intel specific
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- tools/packaging/kata-deploy/local-build/kata-deploy-binaries.sh
+++ tools/packaging/kata-deploy/local-build/kata-deploy-binaries.sh
@@ -357,7 +357,7 @@ install_cc_shimv2() {
 	RUST_VERSION="$(yq r ${versions_yaml} languages.rust.meta.newest-version)"
 	export GO_VERSION
 	export RUST_VERSION
-	export REMOVE_VMM_CONFIGS="acrn fc"
+	export REMOVE_VMM_CONFIGS="acrn fc qemu-sev"
 
         extra_opts="DEFSERVICEOFFLOAD=true"
 	if [ -f "${repo_root_dir}/tools/osbuilder/root_hash_vanilla.txt" ]; then
